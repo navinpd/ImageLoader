fe7feeaa14220ebd699864abd379b195517eed7d
Navin prasad <navin.pd@gmail.com>
2020-03-29 22:49:13 +0800

    Added fragment components and injection

--- app/src/main/java/com/big/imageloader/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/big/imageloader/ui/dashboard/DashboardFragment.kt
@@ -8,11 +8,25 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.big.imageloader.MyApplication
 import com.big.imageloader.R
+import com.big.imageloader.di.DaggerFragmentComponent
+import com.big.imageloader.di.module.DashboardFragmentModule
+import com.squareup.picasso.Picasso
+import javax.inject.Inject
 
 class DashboardFragment : Fragment() {
 
+    @Inject
+    lateinit var picasso: Picasso
+
+
     private lateinit var dashboardViewModel: DashboardViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        getDependencies()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,4 +42,17 @@ class DashboardFragment : Fragment() {
         })
         return root
     }
+
+    private fun getDependencies() {
+        DaggerFragmentComponent
+            .builder()
+            .applicationComponent(
+                (context!!
+                    .applicationContext as MyApplication).applicationComponent
+            )
+            .dashboardFragmentModule(DashboardFragmentModule(this))
+            .build()
+            .inject(this)
+    }
+
 }
--- app/src/main/java/com/big/imageloader/ui/home/HomeFragment.kt
+++ app/src/main/java/com/big/imageloader/ui/home/HomeFragment.kt
@@ -8,11 +8,24 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.big.imageloader.MyApplication
 import com.big.imageloader.R
+import com.big.imageloader.di.DaggerFragmentComponent
+import com.big.imageloader.di.module.HomeFragmentModule
+import com.squareup.picasso.Picasso
+import javax.inject.Inject
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+
+    @Inject
+    lateinit var picasso: Picasso
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        getDependencies()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,4 +41,18 @@ class HomeFragment : Fragment() {
         })
         return root
     }
+
+
+    private fun getDependencies() {
+        DaggerFragmentComponent
+            .builder()
+            .applicationComponent(
+                (context!!
+                    .applicationContext as MyApplication).applicationComponent
+            )
+            .homeFragmentModule(HomeFragmentModule(this))
+            .build()
+            .inject(this)
+    }
+
 }
